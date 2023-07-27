95db2ffeef9e438be44c65ae055a7d8bd67b4a8d
Samarth Shah <s223828778@deakin.edu.au>
2023-07-27 19:55:45 +1000

    Added Server and EJS

--- server.js
+++ server.js
@@ -2,54 +2,11 @@ let express = require('express');
 let app = express();
 let port = process.env.port || 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/')); 
 
 app.get('/', (req,res) => {
-    res.render('index.html');
-});
-
-app.get('/addTwoNumbers',(req,res)=>{ //change this into post call
-    let statusCode = 200;
-    let successMessage = 'Successful!';
-    let num1 = req.query.num1;
-    let num2 = req.query.num2;
-    let result = parseFloat(num1) + parseInt(num2);
-    let payLoad = {message: successMessage, code: statusCode, data: result};
-     
-    res.json(payLoad);
-});
-
-app.get('/subTwoNumbers',(req,res)=>{
-    let statusCode = 200;
-    let successMessage = 'Successful!';
-    let num1 = req.query.num1;
-    let num2 = req.query.num2;
-    let result = parseFloat(num1) - parseInt(num2);
-    let payLoad = {message: successMessage, code: statusCode, data: result};
-     
-    res.json(payLoad);
-});
-
-app.get('/mulTwoNumbers',(req,res)=>{
-    let statusCode = 200;
-    let successMessage = 'Successful!';
-    let num1 = req.query.num1;
-    let num2 = req.query.num2;
-    let result = parseFloat(num1) * parseInt(num2);
-    let payLoad = {message: successMessage, code: statusCode, data: result};
-     
-    res.json(payLoad);
-});
-
-app.get('/divTwoNumbers',(req,res)=>{
-    let statusCode = 200;
-    let successMessage = 'Successful!';
-    let num1 = req.query.num1;
-    let num2 = req.query.num2;
-    let result = parseFloat(num1) / parseInt(num2);
-    let payLoad = {message: successMessage, code: statusCode, data: result};
-     
-    res.json(payLoad);
+    res.render('pages/index.ejs');
 });
 
 
