bcfc13c2718cb2e6880c65124e8c75c8568d431f
Samarth Shah <s223828778@deakin.edu.au>
2023-07-27 20:20:43 +1000

    fix: error in server url

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 let express = require('express');
 let app = express();
-let port = process.env.port || 3000;
 
-app.set('view engine', 'ejs');
+const cats = [{
+    title: 'cat 2',
+    path: 'images/cat-1.jpeg',
+    subTitle: 'About Cat 2',
+    description: 'Description of Cat 2'
+},
+{
+    title: 'cat 3',
+    path: 'images/cat-2.jpeg',
+    subTitle: 'About Cat 3',
+    description: 'Description of Cat 3'
+},
+{
+    title: 'cat 4',
+    path: 'images/cat-3.jpeg',
+    subTitle: 'About Cat 4',
+    description: 'jah fdlkjasdhflkjsadhflkjshd lkjhfaslkdj hfkljsad hklfjha slkjdfhl kasjdhflkjasdhflkjas'
+}];
+
+app.set('view engine','ejs');
 app.use(express.static(__dirname + '/')); 
 
-app.get('/', (req,res) => {
-    res.render('pages/index.ejs');
+app.get('/', function (req,res) {
+    res.render('pages/index', {cats:cats});
 });
 
-
-app.listen(port,()=>{
-    console.log('Server Started');
+let port = process.env.port || 3000;
+app.listen(port, ()=>{
+    console.log('express server started');
 });
